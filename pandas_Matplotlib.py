247ca78208f372c08dcfe7de09dc91b2fac0d75c
ARZakirov <arzakirov@edu.hse.ru>
2026-02-04 15:24:53 +0900

    Добавлен анализ данных

--- pandas_Matplotlib.py
+++ pandas_Matplotlib.py
@@ -11,9 +11,19 @@ with open("events.json", "r") as file:
 # Преобразование в DataFrame
 df = pd.DataFrame(data["events"])
 
-# Визуализация
+# 2. Анализ данных
+# Чтобы посмотреть распределение по типам, можно выделить первую часть поля "signature"
+df['event_type'] = df['signature'].apply(lambda x: x.split()[0])  # Берем первую часть (MALWARE-CNC, EXPLOIT, NETBIOS, INDICATOR-COMPROMISE)
+
+# Посчитаем количество событий каждого типа
+event_counts = df['event_type'].value_counts()
+print("Распределение событий по типам:")
+print(event_counts)
+
+# 3. Визуализация
 plt.figure(figsize=(10, 6))
-sns.countplot(data=df, x="signature")
-plt.title("Распределение типов событий безопасности")
-plt.xticks(rotation=90)  # Поворот подписей оси X для лучшей читаемости
+sns.countplot(data=df, x='event_type', order=event_counts.index, palette='Set2')
+plt.title("Распределение типов событий информационной безопасности")
+plt.xlabel("Тип события")
+plt.ylabel("Количество")
 plt.show()
